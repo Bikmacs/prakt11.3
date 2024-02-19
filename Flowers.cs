201c8cd89dea42b95bf062801bfe47b7772d3f35
Bikmacs <macsim.shmelev@yandex.ru>
2024-02-19 23:12:39 +0500

    11.3

---
+++ Flowers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+    class Flowers
+    {
+        private string name;
+
+        public string Names { get; set; }
+        public string tip { get; set; }
+        public string Ottenok { get; set; }
+        public string Country { get; set; }
+        public int Price { get; set; }
+        public int Heigh { get; set; }
+
+        public int Kolv { get; set; }
+
+        public string pris(string Names, string tip, string Ottenok, string Country, int Price, int heigh)
+        {
+            Flowers flowers = new Flowers();
+            flowers.name = Names;
+            flowers.tip = tip;
+            flowers.Ottenok = Ottenok;
+            flowers.Country = Country;
+            flowers.Price = Price;
+            flowers.Heigh = heigh;
+            flowers.Kolv = Kolv;
+            string inf = flowers.name + flowers.tip + flowers.Ottenok + flowers.Country + flowers.Price + flowers.Heigh;
+            return inf;
+        }
+
+        public string info(string Names, string tip, string Ottenok, string Country, int Price, int heigh)
+        {
+            string inf = $"{Names} {tip} {Ottenok} {Country} {Price} {heigh}";
+            return inf;
+        }
+
+        public string Cena(int Price, int kolv)
+        {
+            int ob = Price * kolv;
+            return $"Цена: {ob}";
+        }
+
+        public string GetSaleInfo(int discountPercent)
+        {
+            double discountedPrice = Price - (Price * discountPercent / 10.0);
+            return $"Скидка {discountPercent}%: Цена со скидкой - {discountedPrice}";
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            
+            Flowers flowers = new Flowers();
+
+            listBox1.Items.Add(flowers.info(textBox1.Text , comboBox1.Text , textBox3.Text , comboBox2.Text , Convert.ToInt32(textBox2.Text) , Convert.ToInt32(textBox4.Text)));
+            listBox1.Items.Add(flowers.Cena(Convert.ToInt32(textBox2.Text),Convert.ToInt32(textBox5.Text)));    
+            
+            int count = listBox1.Items.Count;
+
+            if (count > 10)
+            {
+                DialogResult result = MessageBox.Show(
+                "Отчистить данные?",
+                 "Сообщение",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information,
+                MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+
+                if (result == DialogResult.Yes)
+                { 
+                    listBox1.Items.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Goodbye");
+                    this.Close();
+                }
+               
+                
+            }
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("Название не введено", "ошибка");
+            }
+            
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Flowers flowers = new Flowers();
+            listBox1.Items.Add(flowers.Cena(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox5.Text)));
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
